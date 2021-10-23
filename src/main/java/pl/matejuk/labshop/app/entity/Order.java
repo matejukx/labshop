44ab6d7e659160c694efa4b28ef7135f22acd501
Michal Matejuk <michal.matejuk@ihsmarkit.com>
2021-10-23 21:58:07 +0200

    wip

--- src/main/java/pl/matejuk/labshop/app/entity/Order.java
+++ src/main/java/pl/matejuk/labshop/app/entity/Order.java
@@ -3,6 +3,7 @@ package pl.matejuk.labshop.app.entity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -12,9 +13,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue
     private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "client")
+
     private Client client;
+
+    @Column
     private int price;
+
+    @Column
+    private String description;
+
+    @Column(name = "order_date")
     private LocalDate date;
 }
